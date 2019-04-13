4076305c7b5fa6af34af3444e92c7c815076ea7a
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-04-13 19:37:03 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+	# Create Users
+	  10.times do
+	    user = User.new(
+	      name:     Faker::Name.name,
+	      email:    Faker::Internet.email,
+	      password: Faker::Lorem.characters(10)
+	    )
+	    user.skip_confirmation!
+	    user.save!
+	  end
+	  users = User.all
+    user = User.first
+    	user.skip_confirmation!
+    	user.update_attributes!(
+    	  email: 'guncho@gmail.com',
+    	  password: '12121212',
+    	  name: 'Edy'
+    	  )
+	# Create Items
+  50.times do
+  Item.create!(
+              user:         users.sample,
+              name:         Faker::Lorem.sentence,
+	              created_at:   Faker::Date.backward(7)
+
+	  )
+	end
+	items = Item.all
+
+	puts "Seed finished"
+	puts "#{User.count} users created"
+	puts "#{Item.count} items created"
+  
